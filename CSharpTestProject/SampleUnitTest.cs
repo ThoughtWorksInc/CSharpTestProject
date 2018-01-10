03d592a7d99f9fd72f83e740db4df96d542c6754
damned <writetodan@yahoo.com>
2018-01-10 11:24:56 +0000

    C# dotnet core 2.0 project with xunit and failing test, good for VS 2017

---
+++ CSharpTestProject/SampleUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CSharpTestProject
+{
+    public class SampleUnitTest
+    {
+        [Fact]
+        public void ShouldFail()
+        {
+            Assert.True(false);
+        }
+    }
+}
